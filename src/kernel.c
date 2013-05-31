56f19227520c3c143c16fb31351a070f1824b6da
ioji <ioji@pcescritorio.(none)>
2013-05-31 00:15:59 -0300

    Archivos iniciales

---
+++ src/kernel.c
@@ -0,0 +1,60 @@
+#include "../include/kasm.h"
+#include "../include/defs.h"
+
+DESCR_INT idt[0xA];			/* IDT de 10 entradas*/
+IDTR idtr;				/* IDTR */
+
+
+
+void int_08() {
+
+    //char *video = (char *) 0xb8000;
+    //video[tickpos+=2]='!';
+
+
+}
+
+
+/**********************************************
+kmain() 
+Punto de entrada de código C.
+*************************************************/
+
+kmain() 
+{
+
+        int i, num;
+
+/* Borra la pantalla. */ 
+
+	k_clear_screen();
+
+
+/* CARGA DE IDT CON LA RUTINA DE ATENCION DE IRQ0    */
+
+        setup_IDT_entry (&idt[0x08], 0x08, (dword)&_int_08_hand, ACS_INT, 0);
+	setup_IDT_entry (&idt[0x09], 0x08, (dword)&_int_09_hand, ACS_INT, 0);
+	
+/* Carga de IDTR    */
+
+	idtr.base = 0;  
+	idtr.base +=(dword) &idt;
+	idtr.limit = sizeof(idt)-1;
+	
+	_lidt (&idtr);	
+
+	_Cli();
+/* Habilito interrupcion de timer tick*/
+
+        _mascaraPIC1(0xFC);
+        _mascaraPIC2(0xFF);
+        
+	_Sti();
+
+        while(1)
+        {
+	//print("Hola");
+        }
+	
+}
+
