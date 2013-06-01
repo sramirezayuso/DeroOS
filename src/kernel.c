a78b757e634c8add798106c5dc5df5befdf7ae9b
iojichervo <ioji@chervonagura.com.ar>
2013-06-01 02:25:20 -0300

    Read funcionando

--- src/kernel.c
+++ src/kernel.c
@@ -3,8 +3,8 @@
 #include "../include/kernel.h"
 #include "../include/stdio.h"
 
-DESCR_INT idt[0xA];			/* IDT de 10 entradas*/
-IDTR idtr;				/* IDTR */
+DESCR_INT idt[0xA];                     /* IDT de 10 entradas*/
+IDTR idtr;                              /* IDTR */
 
 
 
@@ -18,101 +18,107 @@ void int_08() {
 
 
 void int_09(int scancode) {
-
-	if(scancode <= 128) {
+    if (scancode == 2){
+        int temp[10];
+        __read(1, (void*) temp, 1);
+        putchar(temp[0]);
+    }
+	
+    if(scancode <= 128) {
 		putchar( (int) getKey(scancode));
-	}
+    }
 }
 
 size_t __write(int fd, const void* buffer, size_t count) {
 
-	size_t cantW = 0;
-	char * b = (char *) buffer;
-	
-	if(count < 0)
-		return -1;
+        size_t cantW = 0;
+        char * b = (char *) buffer;
+        if(count < 0)
+                return -1;
 
-	switch(fd) {
-	
-		case 1:
-				while(count) {
-					_writeScreen(b[cantW], screenPosition(2));
-					count--;
-					cantW++;
-				}
-				
-				return cantW;
-		default:
-				return -1;
-	}
+        switch(fd) {
+
+                case 1:
+                                while(count) {
+                                        _writeScreen(b[cantW], screenPosition(2));
+                                        count--;
+                                        cantW++;
+                                }
+
+                                return cantW;
+                default:
+                                return -1;
+        }
 }
 
 
 
 size_t __read(int fd, void* buffer, size_t count) {
 
-	size_t cantR = 0;
+        size_t cantR = 0;
+        char* cBuf;
+        if(count < 0)
+                return -1;
 
-	if(count < 0)
-		return -1;
+        switch(fd) {
 
-	switch(fd) {
-	
-		case 1:
-				while(count) {
-					_read(buffer + count, screenPosition());
-					count--;
-					cantR++;
-				}
-				
-				return cantR;
-		default:
-				return -1;
-	}
+        case 1:
+            while(count) {
+                char var = _read(0xb8000);
+                cBuf = buffer;
+                cBuf[cantR] = var;
+                count--;
+                cantR++;
+            }
+
+            return cantR;
+        default:
+            return -1;
+    }
 
 }
 
 
+
 /**********************************************
-kmain() 
+kmain()
 Punto de entrada de código C.
 *************************************************/
 
-kmain() 
+void kmain()
 {
 
         int i, num;
 
-/* Borra la pantalla. */ 
+/* Borra la pantalla. */
 
-	k_clear_screen();
+        k_clear_screen();
 
 
 /* CARGA DE IDT CON LA RUTINA DE ATENCION DE IRQ0    */
 
     setup_IDT_entry (&idt[0x08], 0x08, (dword)&_int_08_hand, ACS_INT, 0);
-	setup_IDT_entry (&idt[0x09], 0x08, (dword)&_int_09_hand, ACS_INT, 0);
-	
+        setup_IDT_entry (&idt[0x09], 0x08, (dword)&_int_09_hand, ACS_INT, 0);
+
 /* Carga de IDTR    */
 
-	idtr.base = 0;  
-	idtr.base +=(dword) &idt;
-	idtr.limit = sizeof(idt)-1;
-	
-	_lidt (&idtr);	
+        idtr.base = 0;
+        idtr.base +=(dword) &idt;
+        idtr.limit = sizeof(idt)-1;
 
-	_Cli();
+        _lidt (&idtr);
+
+        _Cli();
 /* Habilito interrupcion de timer tick*/
 
         _mascaraPIC1(0xFC);
         _mascaraPIC2(0xFF);
-        
-	_Sti();
+
+        _Sti();
 
         while(1)
         {
-	//print("Hola");
+        //print("Hola");
         }
-	
-}
 
+}
