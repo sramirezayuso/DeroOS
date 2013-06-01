d9bf27ac8e86a7db5c192774807be9b050a12a5f
iojichervo <ioji@chervonagura.com.ar>
2013-06-01 00:02:23 -0300

    Funciones basicas

---
+++ include/stdio.h
@@ -0,0 +1,19 @@
+/*********************************************
+stdio.h
+
+************************************************/
+
+#define NULL 0
+
+
+int printf ( const char * format, ... );
+
+int scanf ( const char * format, ... );
+
+int getc ( /*FILE * stream*/ );
+
+int putc ( int character/*, FILE * stream */);
+
+int putchar ( int character );
+
+int getchar ( void );
