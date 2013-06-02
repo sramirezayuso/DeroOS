59fb461c09efcb0f0800b42b3480d099f113199c
iojichervo <ioji@chervonagura.com.ar>
2013-06-02 00:14:31 -0300

    printf medio rustico, pero funciona

--- src/stdio.c
+++ src/stdio.c
@@ -1,33 +1,83 @@
 #include "../include/stdio.h"
 
+/* Imprime un caracter por pantalla */
+void printChar (char c);
 
+/* Imprime una cadena de caracteres por pantalla y devuelve la longitud del mismo */
+int print (char* string);
+
+/* Imprime el entero por pantalla y devuelve la longitud del mismo */
+int printInt (int num);
 
 int printf ( const char * format, ... ) {
-return 0;
+	va_list ap;
+	va_start(ap, format);
+
+	char * charVal;
+	char ** stringVal;
+	int * intVal;
+	int charsPrinted = 0;
+	int i;
+	
+	for(i=0 ; format[i] != 0 ; i++) {
+	
+		if(format[i] == '%') {
+		
+			i++;
+			switch(format[i]) {
+			
+				case 'd':
+						intVal = va_arg(ap);
+						charsPrinted += printInt(*intVal);
+						break;				
+				case 'c':
+						charVal = va_arg(ap);
+						charsPrinted++;
+						printChar(*charVal);
+						break;				
+				case 's':
+						stringVal = va_arg(ap);
+						charsPrinted += print(*stringVal);
+						break;						
+				default:
+						break;
+			}
+		
+		} else {
+		
+			printChar(format[i]);
+			charsPrinted++;
+		
+		}
+	
+	
+	}
+	
+	va_end(ap);
+	return charsPrinted;
 }
 
 int scanf ( const char * format, ... ) {
-return 0;
+return NULL;
 }
 
 int getc ( /*FILE * stream */) {
-return 0;
+return NULL;
 }
 
 int putc ( int character/*, FILE * stream*/ ) {
-return 0;
+return NULL;
 }
 
 int putchar ( int character ) {
 	__write(1, &character, 1);
 	return character;
 }
-
+/* ESTO NO FUNCIONA */
 int getchar ( void ) {
-return 0;
+	char * character;
+	return __read(0, (void *) character , 1);
 }
-
-
 
 
 
@@ -41,7 +91,7 @@ void printChar (char c) {
 	__write(1, &c, 1);	
 }
 
-void print (char* string){
+int print (char* string){
     int counter = 0;
 
     while (string[counter] != '\0'){
@@ -51,9 +101,12 @@ void print (char* string){
 
     }
    
+	return counter;
 }
 
-void printInt (int num){
+int printInt (int num){
+	int numLong = 0;
+	
     if(num < 0) {
 	printChar('-');
 	num = num * -1;
@@ -61,10 +114,10 @@ void printInt (int num){
 
     if(num < 10) {
 	printChar('0'+ num);
-	return;
+	return 1;
     }
 
-    printInt(num/10);
+    numLong = printInt(num/10);
     printChar('0'+ (num % 10));
-
+	return numLong++;
 }
