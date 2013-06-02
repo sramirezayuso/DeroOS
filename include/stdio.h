2170f58d6afa879a34639860e515284245912146
iojichervo <ioji@chervonagura.com.ar>
2013-06-02 00:09:07 -0300

    Agregadas las macros para el uso de funciones con argumentos variables

--- include/stdio.h
+++ include/stdio.h
@@ -3,7 +3,15 @@ stdio.h
 
 ************************************************/
 
+#ifndef _stdio_
+#define _stdio_
+
+
 #define NULL 0
+#define va_start(ap, arg) ((ap) = (&arg))
+#define va_arg(ap) ( (ap) += sizeof(int) )
+#define va_end(ap) ((ap) = NULL)
+typedef void* va_list;
 
 
 int printf ( const char * format, ... );
@@ -17,3 +25,6 @@ int putc ( int character/*, FILE * stream */);
 int putchar ( int character );
 
 int getchar ( void );
+
+
+#endif
